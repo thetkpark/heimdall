e67d852d27c325f0f5a68ba005d8dd35341e10fc
Sethanant Pipatpakorn <sethanant.p@linecorp.com>
2022-07-18 16:51:09 +0700

    add: testcase

--- pkg/signature/jws_test.go
+++ pkg/signature/jws_test.go
@@ -1,7 +1,6 @@
 package signature_test
 
 import (
-	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/thetkpark/heimdall/pkg/signature"
@@ -20,11 +19,16 @@ var _ = Describe("Json Web Signature", func() {
 		token, err := jws.Sign(plaintext)
 		Expect(err).To(BeNil())
 		Expect(token).ToNot(BeEmpty())
-		fmt.Println(string(token))
 
 		verified, err := jws.Verify(token)
 		Expect(err).To(BeNil())
 		Expect(verified).To(Equal(plaintext))
-		fmt.Println(string(verified))
+	})
+
+	It("failed to verify when token is edited", func() {
+		token := "eyJhbGciOiJIUzI1NiJ9.TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnRldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWE.Sp4CSEJgAe_AS2Ao2cyO3K17ufOGieZTpjzwrKwHl7U\n\n"
+		verified, err := jws.Verify([]byte(token))
+		Expect(err).ToNot(BeNil())
+		Expect(verified).To(BeNil())
 	})
 })
